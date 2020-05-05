dfccf1c817b6595fbed4633139ad7c5df5135eb3
Crowdbotics <team@crowdbotics.com>
2020-05-05 08:33:49 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/calendr_16617/MainActivity.java
@@ -0,0 +1,15 @@
+package com.calendr_16617;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "calendr_16617";
+    }
+}
